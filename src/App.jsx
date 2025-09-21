91e7c97da520cb56d78757e7e56755ad562127dc
adithyas0047 <adithyasreenath005@gmail.com>
2025-09-21 18:21:21 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NotFound from "./pages/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   );
 }
