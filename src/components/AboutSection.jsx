b158b4f3ee53aacfaea6a065c63a0ae9b10fa6c7
adithyas0047 <adithyasreenath005@gmail.com>
2025-09-29 22:35:17 +0530

    Updated AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -44,7 +44,7 @@ export default function AboutSection() {
               </a>
 
               <a
-                href="/AdithyaS_2.pdf"
+                href="/AdithyaS_3.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 target="_blank"
                 rel="noopener noreferrer"
